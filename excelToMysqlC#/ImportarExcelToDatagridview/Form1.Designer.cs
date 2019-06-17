753ab9392412607c7031afe479bb4a26c3e4ee07
David De La Cruz <delacruz_luis@outlook.es>
2019-06-17 10:45:07 -0500

    Adaptación guardar en notepad- GMS to GD

--- excelToMysqlC#/ImportarExcelToDatagridview/Form1.Designer.cs
+++ excelToMysqlC#/ImportarExcelToDatagridview/Form1.Designer.cs
@@ -29,82 +29,9 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.DataBase = new System.Windows.Forms.TextBox();
-            this.Password = new System.Windows.Forms.TextBox();
-            this.User = new System.Windows.Forms.TextBox();
-            this.Server = new System.Windows.Forms.TextBox();
             this.BBrowse = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(193, 177);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 13);
-            this.label5.TabIndex = 29;
-            this.label5.Text = "Base de Datos";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(196, 134);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 13);
-            this.label4.TabIndex = 28;
-            this.label4.Text = "Contraseña";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(193, 86);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
-            this.label3.TabIndex = 27;
-            this.label3.Text = "Usuario";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label2.Location = new System.Drawing.Point(193, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 26;
-            this.label2.Text = "Servidor";
-            // 
-            // DataBase
-            // 
-            this.DataBase.Location = new System.Drawing.Point(193, 193);
-            this.DataBase.Name = "DataBase";
-            this.DataBase.Size = new System.Drawing.Size(100, 20);
-            this.DataBase.TabIndex = 23;
-            // 
-            // Password
-            // 
-            this.Password.Location = new System.Drawing.Point(193, 150);
-            this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(100, 20);
-            this.Password.TabIndex = 22;
-            // 
-            // User
-            // 
-            this.User.Location = new System.Drawing.Point(193, 102);
-            this.User.Name = "User";
-            this.User.Size = new System.Drawing.Size(100, 20);
-            this.User.TabIndex = 21;
-            // 
-            // Server
-            // 
-            this.Server.Location = new System.Drawing.Point(193, 53);
-            this.Server.Name = "Server";
-            this.Server.Size = new System.Drawing.Size(100, 20);
-            this.Server.TabIndex = 20;
             // 
             // BBrowse
             // 
@@ -144,33 +71,16 @@
             this.ClientSize = new System.Drawing.Size(489, 274);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.BBrowse);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.DataBase);
-            this.Controls.Add(this.Password);
-            this.Controls.Add(this.User);
-            this.Controls.Add(this.Server);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Excel to MySQL";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox DataBase;
-        private System.Windows.Forms.TextBox Password;
-        private System.Windows.Forms.TextBox User;
-        private System.Windows.Forms.TextBox Server;
         internal System.Windows.Forms.Button BBrowse;
         private System.Windows.Forms.Button button2;
     }
--- excelToMysqlC#/ImportarExcelToDatagridview/Form1.cs
+++ excelToMysqlC#/ImportarExcelToDatagridview/Form1.cs
@@ -1,5 +1,5 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -10,32 +10,39 @@ namespace ImportarExcelToDatagridview
     public partial class Form1 : Form
     {
         string Label1;
+        #region GuardarArchivo
+        StreamWriter str = new StreamWriter(@"archivo.txt");
+        string query = "";
+        double divMin = 0;
+        double divSeg = 0;
+        //------
+        double divMin1 = 0;
+        double divSeg1 = 0;
+        int latitud = 0;
+        int longitud = 0;
+        double minutos = 60;
+        double segundos = 3600;
+
+        //------------------------DATOS PARA EL CICLO FOR
+        double m1 = 0;
+        double m11 = 0;
+        double m2 = 0;
+        double m22 = 0;
+        double m3 = 0;
+        double m33 = 0;
+        //-----------------------------------------------
+        string digitos = "10000";
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
 
         }
-     
-
-       
-
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            Server.Text = "localhost";
-            DataBase.Text = "";
-            User.Text = "root";
-            Password.Text = "";
-            DataBase.Text = "lalala";
             
-
         }
-        
-
-       
-
-        
-
         private void BBrowse_Click(object sender, EventArgs e)
         {
 
@@ -54,13 +61,12 @@ namespace ImportarExcelToDatagridview
 
         private void button2_Click_1(object sender, EventArgs e)
         {
-
             int CONT = 1;
 
             var excel = new Excel.Application();
             Excel.Workbook librodetrabajo = excel.Workbooks.Open(Label1);
-            string conString = @"Data Source=" + Server.Text + ";Initial Catalog=" + DataBase.Text + ";User Id=" + User.Text + ";password=" + Password.Text;
-            string segundacadena = conString;
+            //string conString = @"Data Source=" + Server.Text + ";Initial Catalog=" + DataBase.Text + ";User Id=" + User.Text + ";password=" + Password.Text;
+            //string segundacadena = conString;
             foreach (Microsoft.Office.Interop.Excel.Worksheet Hojas in librodetrabajo.Sheets)
             {
                 Excel._Worksheet xlWorksheet = librodetrabajo.Sheets[CONT];
@@ -70,101 +76,118 @@ namespace ImportarExcelToDatagridview
                 int rowCount = xlRange.Rows.Count;
                 int colCount = xlRange.Columns.Count;
 
-                string okf = "";
-                string POS = "";
-                for (int i = 1; i <= colCount; i++)
-                {
-                    POS += "`" + xlRange.Cells[1, i].Value2.ToString() + "`,";
-             
-                }
-                okf = POS.Substring(0, POS.Length - 1);
+                //string okf = "";
+                //string POS = "";
+                //for (int i = 1; i <= colCount; i++)
+                //{
+                //    POS += "`" + xlRange.Cells[1, i].Value2.ToString() + "`,";
+
+                //}
+                //okf = POS.Substring(0, POS.Length - 1);
                 
-
-                string value = "";
-                string result = "";
-
-                value = "";
-                
-                
-
-
                 for (int b = 2; b <= rowCount; b++)
                 {
-                    value = "";
-                    result = "";
-                    
+                    string value = "";
+                    string resultExcel = "";
                     for (int c = 1; c <= colCount; c++)
                     {
-                       
-                        value += "\"" + xlRange.Cells[b, c].Value2.ToString() + "\",";                       
-                       result = value.Substring(0, value.Length - 1);
-                        
 
-                  }
-
-                    
-
-
-                    string[] campos = okf.Split(',');
-                    string[] valores = result.Split(',');
-
-
-                    string wek = "";
-                    string query2 = "Select count(*) from " + Hojas.Name + " where ";
-
-                    for (int j = 0; j < 1; j++)
+                        value +=xlRange.Cells[b, c].Value2.ToString() + ",";
+                        resultExcel = value.Substring(0, value.Length - 1);
+                    }
+                    string[] valores = resultExcel.Split(',');
+                    string contratoarea = valores[0];
+                    string verticePoligono = valores[1];
+                    latitud = Convert.ToInt32(valores[3]);
+                    longitud = Convert.ToInt32(valores[2]);
+                    m1 = latitud / 10000; //Primeros 2 números
+                    int m4 = latitud % Convert.ToInt32(digitos);
+                    string result = m4.ToString();
+                    int ZeroConditional = int.Parse(result);
+                    string sub = "";
+                    if (ZeroConditional == 0)
                     {
-                        query2 = "Select count(*) from " + Hojas.Name + " where ";
-
-                        for (int i = 0; i < 3; i++)
+                        try
                         {
-
-                            query2 += campos[i] + "=" + valores[i] + " and ";
-
-
+                            divMin = 0;
                         }
-
-                        wek = query2.Substring(0, query2.Length - 4);
-                        int ifs = 0;
-                        using (var conn = new MySqlConnection(segundacadena))
+                        catch (Exception ex)
                         {
-                            conn.Open();
-                            using (var cmd = new MySqlCommand(wek, conn))
+
+                            throw ex;
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            sub = result.Substring(0, result.Length - 2); // Segundos dos números de en medio
+                            if (sub =="")
                             {
-                                int count = Convert.ToInt32(cmd.ExecuteScalar());
-
-                                ifs = count;
+                                sub = "0";
                             }
-                            conn.Close();
                         }
-                        if (ifs == 1)
+                        catch (Exception ex)
                         {
 
+                            throw ex;
                         }
-                        if (ifs == 0)
-                        {
-
-                            string Query = "insert into " + Hojas.Name + "(" + okf + " ) values(" + result + ")";
-                            MessageBox.Show(Query);
-                            MySqlConnection MyConn2 = new MySqlConnection(conString);
-                            MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
-                            MySqlDataReader MyReader2;
-                            MyConn2.Open();
-                            MyReader2 = MyCommand2.ExecuteReader();
-                            MyConn2.Close();
-                        }
-
+                    }
+                    m2 = (latitud % (10000));
+                    m3 = latitud % 100;
+                    //--------------------------------------------
+                    //Console.WriteLine(m1+" "+sub+" "+m3);
+                    if (ZeroConditional == 0)
+                    {
+                        divMin = 0;
+                    }
+                    else
+                    {
+                        divMin = Convert.ToDouble(sub) / minutos; //Números de en medio entre minutos
                     }
 
+                    divSeg = m3 / segundos;
+                    //--------------Asignar valor  a LATITUD Y LONGITUD
+
+                    //Console.WriteLine(m1 + " " + divMin + " " + divSeg);
+                    double suma = m1 + divMin + divSeg;
+                    if (contratoarea == "CNH-R01-L03-A18/2015" && verticePoligono =="10")
+                    {
+
+                    }
+                    m11 = longitud / 10000;
+                    int m44 = longitud % Convert.ToInt32(digitos);
+                    string result2 = m44.ToString();
+                    string sub1 = "";
+                    if (result2 == "0")
+                    {
+                        sub1 = "0";
+                    }
+                    else
+                    {
+                        sub1=result2.Substring(0, result2.Length - 2);
+                        if (sub1=="")
+                        {
+                            sub1 = "0";
+                        }
+                    }
+                    
+                    m22 = (longitud % (10000));
+                    m33 = longitud % 100;
+                    //--------------------------------------------
+                    //Console.WriteLine(m1+" "+sub+" "+m3);
+                    divMin1 = Convert.ToDouble(sub1) / minutos;
+                    divSeg1 = m33 / segundos;
+                    //--------------Asignar valor  a LATITUD Y LONGITUD
+
+                    //Console.WriteLine(m1 + " " + divMin + " " + divSeg);
+                    double suma2 = m11 + divMin1 + divSeg1;
+                    query = $"insert into CO_Coordenadas (IdAreaContractual,lat,lng, poligono) values ({contratoarea},'{suma}','-{suma2}',{verticePoligono})";
+                    str.WriteLine(query);
                 }
             }
-            string proc = "call proc2()";
-            MySqlConnection MyConn1 = new MySqlConnection(conString);
-            MySqlCommand MyCommand1 = new MySqlCommand(proc, MyConn1);
-            MyConn1.Open();
-            var cmd1 = new MySqlCommand(proc, MyConn1);
-            MyConn1.Close();
-           
+            str.Close();
+            librodetrabajo.Close();
             MessageBox.Show("final");
         }
     }
